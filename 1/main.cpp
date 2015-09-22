d54f9e5739afd42a6a7e86a936dacddc4e6f3fe8
LatushkinaMargarita <marlatuha@mail.ru>
2015-09-22 18:44:28 +0300

    Update main.cpp

--- 1/main.cpp
+++ 1/main.cpp
@@ -9,22 +9,9 @@
 #include <iostream>
 #include <stdio.h>
 using namespace std;
-
-int factor (int n);
-
-int main()
-{
-   int n;
-    cout<<"vvedite chislo ";
-    cin>>n;
-    cout<<"otvet "<<factor(n);
-}
-
-
-
 int factor (int n)
 {
-    int g = 0;
+	int g = 0;
     for (int i = 1; i <= n; i++)
     {
         for (int k = 5; k < n; k *= 5 )
@@ -35,6 +22,28 @@ int factor (int n)
             }
         }
     }
-    
-    return g;
+       return g;
+}
+
+
+
+void assert ( int n, int result)
+{
+	
+	if (factor(n)==result)
+	{
+		cout << "Ok" << endl;
+	}
+	else 
+		cout <<"Fail " << endl;
+}
+int main()
+{
+   assert(3,0);
+    assert(5,1);
+    assert(7,1);
+    assert(10,2);
+    assert(15,3);
+	system("pause");
+    return 0;
 }
